9cf524300371024b071ed3cbf452d2ad0a03b85b
s4:D_t0y <roccowu949@gmail.com>
2024-02-07 20:06:39 -0800

    Implementing clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{command, Arg, ArgAction};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = command!()
+        .arg(
+            Arg::new("verbose")
+                .long("verbose")
+                .short('v')
+                .action(ArgAction::Count)
+        )
+        .get_matches();
+
+    let verbose = matches.get_count("verbose");
+    println!("Verbosity level at {}", verbose);
 }
